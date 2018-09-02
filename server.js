b9111b14a3db4286a247b7cafc09b31e9d245bb1
Winton Liu <33133071+winron@users.noreply.github.com>
2018-09-02 14:29:54 -0400

    Update server.js
    added code for bootcamp1

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var listingData, server;
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
+
   /*
     Your request handler should send listingData in the JSON format if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
@@ -16,11 +17,41 @@ var requestHandler = function(request, response) {
     HINT: explore the request object and its properties 
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
+
+    //print listingData if met requirements
+    if (request.method === "GET" && parsedUrl.pathname === "/listings") {
+        response.statusCode = '200';
+        response.write(listingData);
+        response.end();
+    }
+
+    //print error statement if failed requirements
+    else {
+    response.statusCode = '404';
+    response.write("Bad gateway error");
+    response.end();
+    }
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
+    
+/*
     This callback function should save the data in the listingData variable, 
     then start the server. 
    */
+
+    //print error statement if failed to read file.
+    if(err) {
+    console.log("error reading json!");
+    return;
+}
+
+    //save data as listingData
+    listingData = data; 
+
+    //start server
+    server = http.createServer(requestHandler);
+    server.listen(port);
+    console.log('server listening on: http://localhost:' + port);
+
 });
